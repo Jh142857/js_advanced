e51c6be721cdf3862048c4512dccec435124ffb3
chenjunhao <609752056@qq.com>
2022-08-28 10:17:26 +0800

    add js_advanced_material

--- 20220827/01-切换栏制作/js/index.js
+++ 20220827/01-切换栏制作/js/index.js
@@ -1,4 +1,40 @@
+var that;
 class Tab {
-    constructor()
+    constructor(id) {
+        that = this;
+        this.tab = document.querySelector(id);
+        this.navs = this.tab.querySelector(".tab-nav").querySelectorAll("li");
+        this.cons = this.tab.querySelector(".tab-content").querySelectorAll("li");
+        this.init();
+    }
+    // 绑定事件
+    init() {
+        for (var i = 0; i < this.navs.length; i++) {
+            this.navs[i].index = i;
+            this.navs[i].onclick = this.toggleTab;
+        }
+    }
+    // 切换
+    toggleTab() {
+        that.clearTab();
+        this.className = 'nav-current';
+        that.cons[this.index].className = 'con-current';
+    }
+
+    clearTab() {
+        for(var i = 0; i < this.navs.length; i++) {
+            this.navs[i].className = '';
+            this.cons[i].className = '';
+        }
+    }
+    addTab() {
+
+    }
+    removeTab() {
+
+    }
+    editTab() {
+
+    }
 }
 new Tab('#tab');
